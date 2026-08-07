e47f022a08b43c0c37a1deb70168d59d1dbdd2be
241B239 <241B239@juet.co.in>
2026-08-07 15:54:13 +0530

    ap lab 1

---
+++ Animal.java
@@ -0,0 +1,14 @@
+class Animal{
+    private int age;
+    void setAge(int age){
+        this.age = age;    
+    }
+    
+    int getAge(){
+        return age;
+    }
+    
+    void show (){
+        System.out.println("animal");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+        Cow cow = new Cow();
+        cow.setAge(5);
+        System.out.println(cow.getAge());
+	    
+	}
+}
